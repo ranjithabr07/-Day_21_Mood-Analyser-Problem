ee9db774100b8ffb0165f0053e254addcf512dd2
ranjithabr07 <ranjitharanjubrhlk1998@gmail.com>
2022-10-27 17:47:02 +0530

    Empty mood throw custom exception

---
+++ ExceptionMoodAnalyser/ExceptionMoodAnalyser/MoodAnalyserCustomException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionMoodAnalyser
+{
+    public class MoodAnalyserException : Exception
+    {
+        public enum ExceptionType
+        {
+            EMPTY_MOOD,
+            NULL_MOOD,
+
+        }
+        public ExceptionType exceptionType;
+        public MoodAnalyserException(ExceptionType exceptionType, string message) : base(message)
+        {
+            this.exceptionType = exceptionType;
+        }
+    }
+}
--- ExceptionMoodAnalyser/TestProject1/UnitTest1.cs
+++ ExceptionMoodAnalyser/TestProject1/UnitTest1.cs
@@ -67,6 +67,39 @@ namespace TestProject1
             string message = moodAnalyser.AnalyseMood();
             Assert.AreEqual("SAD", message);
         }
+        // <summary>
+        /// tc 3.1 Given NULL Mood Should Throw MoodAnalysisException
+        /// </summary>
+        [Test]
+        public void GivenMessage_WhenNull_USingCustomException_ShouldReturnNullMood()
+        {
+            moodAnalyser = new MoodAnalyser();
+            try
+            {
+                string message = moodAnalyser.AnalyseMood();
+            }
+            catch (MoodAnalyserException exception)
+            {
+                Assert.AreEqual(MoodAnalyserException.ExceptionType.NULL_MOOD, exception.exceptionType);
+            }
+
+        }
+        // <summary>
+        /// tc 3.2 Given Empty Mood Should Throw MoodAnalysisException indicating Empty Mood
+        /// </summary>
+        [Test]
+        public void GivenMessage_WhenEmpty_USingCustomException_ShouldReturnNullMood()
+        {
+            moodAnalyser = new MoodAnalyser("");
+            try
+            {
+                string message = moodAnalyser.AnalyseMood();
+            }
+            catch (MoodAnalyserException exception)
+            {
+                Assert.AreEqual(MoodAnalyserException.ExceptionType.EMPTY_MOOD, exception.exceptionType);
+            }
+        }
 
     }
 
