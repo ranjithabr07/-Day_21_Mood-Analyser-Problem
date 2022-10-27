ca3b49d0755d87914022c26e89e208d1434d6b7f
ranjithabr07 <ranjitharanjubrhlk1998@gmail.com>
2022-10-27 17:39:19 +0530

    Handle Exception is user provides inavlid Mood

--- ExceptionMoodAnalyser/TestProject1/UnitTest1.cs
+++ ExceptionMoodAnalyser/TestProject1/UnitTest1.cs
@@ -38,7 +38,36 @@ namespace TestProject1
         // <summary>
         /// repeat TC1.1 Given “I am in SAD Mood” message in Constructor Should Return SAD
         /// </summary>
-
+        [Test]
+        public void GivenMessage_WhenSad_ShouldReturnHappy()
+        {
+            moodAnalyser = new MoodAnalyser();
+            string message = moodAnalyser.AnalyseMood();
+            Assert.AreEqual("SAD", message);
+        }
+        // <summary>
+        /// repeat TC1.2 Given “I am in Happy Mood” message in Constructor Should Return HAPPY
+        /// </summary>
+        [Test]
+        public void GivenMessage_WhenHappy_ShouldReturnHappy()
+        {
+            moodAnalyser = new MoodAnalyser("I am in Happy mood");
+            string message = moodAnalyser.AnalyseMood();
+            Assert.AreEqual("HAPPY", message);
+        }
+        //UC-2
+        /// <summary>
+        /// TC2.1Given Null Mood Should Return Happy To make this Test Case pass Handle
+        /// NULL Scenario using try catch and return Happy
+        /// </summary>
+        [Test]
+        public void GivenMessage_WhenNull_ShouldReturnHappy()
+        {
+            moodAnalyser = new MoodAnalyser();
+            string message = moodAnalyser.AnalyseMood();
+            Assert.AreEqual("SAD", message);
+        }
 
     }
+
 }
